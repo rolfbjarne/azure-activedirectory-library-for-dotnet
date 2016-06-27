85c48a251ed1914b75bbc24999335a2e46525bcb
Chris Mann <chrmann@microsoft.com>
2016-06-27 09:34:45 -0700

    Add a datamember to the status so that it is serialized properly

--- src/ADAL.PCL/AuthorizationResult.cs
+++ src/ADAL.PCL/AuthorizationResult.cs
@@ -69,6 +69,7 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
             }
         }
 
+        [DataMember]
         public AuthorizationStatus Status { get; private set; }
 
         [DataMember]
